98e6fa058afdde087ef86c9cbdbd39f205e554af
Shi Yin <joysword@me.com>
2013-09-13 21:55:50 -0500

    adding community locations

---
+++ community list.py
@@ -0,0 +1,88 @@
+areas="""
+Far North Side
+	01 	Rogers Park 42.01, -87.67
+	02 	West Ridge 42, -87.69
+	03 	Uptown 41.97, -87.66
+	04 	Lincoln Square 41.97, -87.69
+	09	Edison Park 42.01, -87.81
+	10	Norwood Park 41.98, -87.8
+	11	Jefferson Park 41.98, -87.77
+	12	Forest Glen 41.983333, -87.75
+	13	North 41.95, -87.68
+	14	Albany Park 41.97, -87.72
+	76	"O'Hare" 42, -87.92
+	77	Edgewater 41.99, -87.66
+Northwest Side
+	15	Portage Park 41.95, -87.76
+	16	Irving Park 41.95, -87.73
+	17	Dunning 41.95, -87.81
+	18	Montclare 41.93, -87.8
+	19	Belmont Cragin 41.93, -87.76
+	20	Hermosa 41.92, -87.73
+North Side
+	5 	North Center 41.95, -87.68
+	6 	Lake View 41.94352, -87.654102
+	7	Lincoln Park 41.92, -87.65
+	21	Avondale 41.94, -87.71
+	22	Logan Square 41.928333, -87.706667
+West Side
+	23	Humboldt Park 41.902809, -87.720886
+	24	West Town 41.9, -87.68
+	25	Austin 41.9, -87.76
+	26	West Garfield Park 41.88, -87.73
+	27	East Garfield Park 41.88, -87.7
+	28	Near West Side 41.87, -87.67
+	29	North Lawndale 41.86, -87.71
+	30	South Lawndale 41.85, -87.71
+	31	Lower West Side 41.85, -87.66
+Central
+	08 	Near North Side 41.9, -87.63
+	32	Loop 41.883333, -87.633333
+	33	Near South Side 41.85, -87.62
+South Side
+	34	Armour Square 41.833333, -87.633333
+	35	Douglas 41.83, -87.62
+	36	Oakland 41.82, -87.6
+	37	Fuller Park 41.81, -87.626667
+	38	Grand Boulevard 41.81, -87.62
+	39	Kenwood 41.81, -87.6
+	40	Washington Park 41.79, -87.62
+	41	Hyde Park 41.79, -87.62
+	42	Woodlawn 41.78, -87.6
+	43	South Shore 41.76, -87.58
+	60	Bridgeport 41.84, -87.65
+	69	Greater Grand Crossing 41.76, -87.61
+Southwest Side
+	56	Garfield Ridge 41.816667, -87.76
+	57	Archer Heights 41.81, -87.73
+	58	Brighton Park 41.82, -87.7
+	59	McKinley Park 41.83, -87.67
+	61	New City 41.81, -87.66
+	62	West Elsdon 41.79, -87.72
+	63	Gage Park 41.79, -87.69
+	64	Clearing 41.78, -87.76
+	65	West Lawn 41.77, -87.72
+	66	Chicago Lawn 41.77, -87.69
+	67	West Englewood 41.78, -87.67
+	68	Englewood 41.779786, -87.644778
+Far Southwest Side
+	70	Ashburn 41.75, -87.71
+	71	Auburn Gresham 41.74, -87.66
+	72	Beverly 41.71, -87.68
+	73	Washington Heights 41.72, -87.65
+	74	Mount Greenwood 41.7, -87.71
+	75	Morgan Park 41.69, -87.67
+Far Southeast Side
+	44	Chatham 41.74, -87.611667
+	45	Avalon Park 41.75, -87.59
+	46	South Chicago 41.74, -87.55
+	47	Burnside 41.73, -87.6
+	48	Calumet Heights 41.728333, -87.579722
+	49	Roseland 41.71, -87.62
+	50	Pullman 41.71, -87.62
+	51	South Deering 41.71, -87.56
+	52	East Side 41.7, -87.56
+	53	West Pullman 41.68, -87.63
+	54	Riverdale 41.66, -87.61
+	55	Hegewisch 41.66, -87.55
+""" 
--- simpleearth.py
+++ simpleearth.py
@@ -61,12 +61,19 @@ TYPE = set(["KIDNAPPING",
 
 class community:
 	name = ''
+	lat = 0
+	lon = 0
 	x = 0
 	y = 0
+	z = 0
 	def __init__(self, n, x, y):
+		temp = llh2ecef(x,y,0)
 		self.name = n
-		self.x = x
-		self.y = y
+		self.lat = x
+		self.lon = y
+		self.x = temp[0]
+		self.y = temp[1]
+		self.z = temp[2]
 	def watchme(self):
 		print ("watching community %s" %(self.name))
 
@@ -78,79 +85,79 @@ comm.insert(1, community("Rogers Park", 42.01, -87.67))
 comm.insert(2, community("West Ridge", 42, -87.69))
 comm.insert(3, community("Uptown", 41.97, -87.66))
 comm.insert(4, community("Lincoln Square", 41.97, -87.69))
-	09	Edison Park 42.01, -87.81
-	10	Norwood Park 41.98, -87.8
-	11	Jefferson Park 41.98, -87.77
-	12	Forest Glen 41.983333, -87.75
-	13	North 41.95, -87.68
-	14	Albany Park 41.97, -87.72
-	76	"O'Hare" 42, -87.92
-	77	Edgewater 41.99, -87.66
-	15	Portage Park 41.95, -87.76
-	16	Irving Park 41.95, -87.73
-	17	Dunning 41.95, -87.81
-	18	Montclare 41.93, -87.8
-	19	Belmont Cragin 41.93, -87.76
-	20	Hermosa 41.92, -87.73
-	5 	North Center 41.95, -87.68
-	6 	Lake View 41.94352, -87.654102
-	7	Lincoln Park 41.92, -87.65
-	21	Avondale 41.94, -87.71
-	22	Logan Square 41.928333, -87.706667
-	23	Humboldt Park 41.902809, -87.720886
-	24	West Town 41.9, -87.68
-	25	Austin 41.9, -87.76
-	26	West Garfield Park 41.88, -87.73
-	27	East Garfield Park 41.88, -87.7
-	28	Near West Side 41.87, -87.67
-	29	North Lawndale 41.86, -87.71
-	30	South Lawndale 41.85, -87.71
-	31	Lower West Side 41.85, -87.66
-	08 	Near North Side 41.9, -87.63
-	32	Loop
-	33	Near South Side 41.85, -87.62
-	34	Armour Square 41.833333, -87.633333
-	35	Douglas 41.83, -87.62
-	36	Oakland 41.82, -87.6
-	37	Fuller Park 41.81, -87.626667
-	38	Grand Boulevard 41.81, -87.62
-	39	Kenwood 41.81, -87.6
-	40	Washington Park 41.79, -87.62
-	41	Hyde Park 41.79, -87.62
-	42	Woodlawn 41.78, -87.6
-	43	South Shore 41.76, -87.58
-	60	Bridgeport 41.84, -87.65
-	69	Greater Grand Crossing 41.76, -87.61
-	56	Garfield Ridge 41.816667, -87.76
-	57	Archer Heights 41.81, -87.73
-	58	Brighton Park 41.82, -87.7
-	59	McKinley Park 41.83, -87.67
-	61	New City 41.81, -87.66
-	62	West Elsdon 41.79, -87.72
-	63	Gage Park 41.79, -87.69
-	64	Clearing 41.78, -87.76
-	65	West Lawn 41.77, -87.72
-	66	Chicago Lawn 41.77, -87.69
-	67	West Englewood 41.78, -87.67
-	68	Englewood 41.779786, -87.644778
-	70	Ashburn 41.75, -87.71
-	71	Auburn Gresham 41.74, -87.66
-	72	Beverly 41.71, -87.68
-	73	Washington Heights 41.72, -87.65
-	74	Mount Greenwood 41.7, -87.71
-	75	Morgan Park 41.69, -87.67
-	44	Chatham 41.74, -87.611667
-	45	Avalon Park 41.75, -87.59
-	46	South Chicago 41.74, -87.55
-	47	Burnside 41.73, -87.6
-	48	Calumet Heights 41.728333, -87.579722
-	49	Roseland 41.71, -87.62
-	50	Pullman 41.71, -87.62
-	51	South Deering 41.71, -87.56
-	52	East Side 41.7, -87.56
-	53	West Pullman 41.68, -87.63
-	54	Riverdale 41.66, -87.61
-	55	Hegewisch 41.66, -87.55
+comm.insert(9, community("Edison Park", 42.01, -87.81))
+comm.insert(10, community("Norwood Park", 41.98, -87.8))
+comm.insert(11, community("Jefferson Park", 41.98, -87.77))
+comm.insert(12, community("Forest Glen", 41.983333, -87.75))
+comm.insert(13, community("North", 41.95, -87.68))
+comm.insert(14, community("Albany Park", 41.97, -87.72))
+comm.insert(76, community("O'Hare", 42, -87.92))
+comm.insert(77, community("Edgewater", 41.99, -87.66))
+comm.insert(15, community("Portage Park", 41.95, -87.76))
+comm.insert(16, community("Irving Park", 41.95, -87.73))
+comm.insert(17, community("Dunning", 41.95, -87.81))
+comm.insert(18, community("Montclare", 41.93, -87.8
+comm.insert(19, community("Belmont Cragin", 41.93, -87.76
+comm.insert(20, community("Hermosa", 41.92, -87.73
+comm.insert(5 , community("North Center", 41.95, -87.68
+comm.insert(6 , community("Lake View", 41.94352, -87.654102
+comm.insert(7, community("Lincoln Park", 41.92, -87.65
+comm.insert(21, community("Avondale", 41.94, -87.71
+comm.insert(22, community("Logan Square", 41.928333, -87.706667
+comm.insert(23, community("Humboldt Park", 41.902809, -87.720886
+comm.insert(24, community("West Town", 41.9, -87.68
+comm.insert(25, community("Austin", 41.9, -87.76
+comm.insert(26, community("West Garfield Park", 41.88, -87.73
+comm.insert(27, community("East Garfield Park", 41.88, -87.7
+comm.insert(28, community("Near West Side 41.87, -87.67
+comm.insert(29, community("North Lawndale 41.86, -87.71
+comm.insert(30, community("South Lawndale 41.85, -87.71
+comm.insert(31, community("Lower West Side 41.85, -87.66
+comm.insert(08 , community("Near North Side 41.9, -87.63
+comm.insert(32, community("Loop
+comm.insert(33, community("Near South Side 41.85, -87.62
+comm.insert(34, community("Armour Square 41.833333, -87.633333
+comm.insert(35, community("Douglas 41.83, -87.62
+comm.insert(36, community("Oakland 41.82, -87.6
+comm.insert(37, community("Fuller Park 41.81, -87.626667
+comm.insert(38, community("Grand Boulevard 41.81, -87.62
+comm.insert(39, community("Kenwood 41.81, -87.6
+comm.insert(40, community("Washington Park 41.79, -87.62
+comm.insert(41, community("Hyde Park 41.79, -87.62
+comm.insert(42, community("Woodlawn 41.78, -87.6
+comm.insert(43, community("South Shore 41.76, -87.58
+comm.insert(60, community("Bridgeport 41.84, -87.65
+comm.insert(69, community("Greater Grand Crossing 41.76, -87.61
+comm.insert(56, community("Garfield Ridge 41.816667, -87.76
+comm.insert(57, community("Archer Heights 41.81, -87.73
+comm.insert(58, community("Brighton Park 41.82, -87.7
+comm.insert(59, community("McKinley Park 41.83, -87.67
+comm.insert(61, community("New City 41.81, -87.66
+comm.insert(62, community("West Elsdon 41.79, -87.72
+comm.insert(63, community("Gage Park 41.79, -87.69
+comm.insert(64, community("Clearing 41.78, -87.76
+comm.insert(65, community("West Lawn 41.77, -87.72
+comm.insert(66, community("Chicago Lawn 41.77, -87.69
+comm.insert(67, community("West Englewood 41.78, -87.67
+comm.insert(68, community("Englewood 41.779786, -87.644778
+comm.insert(70, community("Ashburn 41.75, -87.71
+comm.insert(71, community("Auburn Gresham 41.74, -87.66
+comm.insert(72, community("Beverly 41.71, -87.68
+comm.insert(73, community("Washington Heights 41.72, -87.65
+comm.insert(74, community("Mount Greenwood 41.7, -87.71
+comm.insert(75, community("Morgan Park 41.69, -87.67
+comm.insert(44, community("Chatham 41.74, -87.611667
+comm.insert(45, community("Avalon Park 41.75, -87.59
+comm.insert(46, community("South Chicago 41.74, -87.55
+comm.insert(47, community("Burnside 41.73, -87.6
+comm.insert(48, community("Calumet Heights 41.728333, -87.579722
+comm.insert(49, community("Roseland 41.71, -87.62
+comm.insert(50, community("Pullman 41.71, -87.62
+comm.insert(51, community("South Deering 41.71, -87.56
+comm.insert(52, community("East Side 41.7, -87.56
+comm.insert(53, community("West Pullman 41.68, -87.63
+comm.insert(54, community("Riverdale 41.66, -87.61
+comm.insert(55,"Hegewisch 41.66, -87.55
 
 ##############################################################################################################
 # CREATE MENUS
